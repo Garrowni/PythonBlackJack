78a128876e31649d2f57a391e64240e2e0a216dc
Garrowni <nicole_dg@hotmail.ca>
2023-10-11 12:05:30 +0000

    added suits

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-suit = 'hearts'
+suits = ["spades", "clubs", "hearts", "diamonds"]
+suit = suits[1]
 rank = 'K'
 value = 10
 
-print(f"Your card is: {rank}")
+print(f"Your card is: {rank} of {suit}")
+for suit in suits: print(suit)
